4400ac0597cc2b75b186cfe16a8936f2d55e0206
sezercakir <cakirta18@itu.edu.tr>
2022-03-12 18:08:05 +0300

    main added

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, redirect, abort, jsonify, request
 from Database.database import Database
 from url.url import URL
+from url.views import * 
 
-
-app         = Flask(__name__)
-db          = Database("ulr_databse.db")
-url_obj     = URL()
-
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
